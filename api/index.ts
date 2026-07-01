70c19edf0124f655a7eacf2b248e08fad0019f89
Aljayve <capara.072094@balagtas.sti.edu.ph>
2026-07-01 16:48:52 +0800

    Edit api/index.ts

--- api/index.ts
+++ api/index.ts
@@ -22,7 +22,21 @@ async function bootstrap() {
 
 let cachedServer: express.Express;
 
+function setCorsHeaders(res: any) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+}
+
 export default async function handler(req: any, res: any) {
+    setCorsHeaders(res);
+
+    if (req.method === "OPTIONS") {
+        res.status(204).end();
+        return;
+    }
+
     if (!cachedServer) {
         cachedServer = await bootstrap();
     }
